ba606ac6827aa252230687120e30a5e36b102d96
MacBook Air M2 <macbookairm2@siams-macBook-Air.local>
2025-12-08 02:23:28 +0600

    db confiq & tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import path from "path";
+import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./config/env.js";
+import { connectDB } from './config/db.js';
 
 const app = express()
 const __dirname = path.resolve();
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 app.get('/api/health',(req,res)=>{
     res.status(200).json({message:'Success'})
 })
@@ -15,4 +18,7 @@ if (ENV.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
   });
 }
-app.listen(ENV.PORT, () => console.log("Server is up and running"))
+app.listen(ENV.PORT, () => {console.log(
+    "Server is up and running")
+    connectDB()
+})    
